ebf6025b8fbcef6df7f84aacb3d4e8b4f8225d7a
mardomai <mardo.mai@ametikool.ee>
2023-12-04 09:49:03 +0200

    yes

---
+++ day1/day1.py
@@ -0,0 +1,22 @@
+import re
+
+input = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+if __name__ == "__main__":
+    sum = 0
+
+    for line in input.splitlines():
+        digits = re.findall(r'\d', line)
+        first = digits[0]
+        last = digits[-1]
+        linedigits = f"{first}{last}"
+        sum += int(linedigits)
+        #(linedigits)
+
+    print(f"Result: {sum}")
